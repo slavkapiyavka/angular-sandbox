64cc3c127d8dcd8f1ac66ba19f7ca6069c2b993a
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2025-03-01 00:57:31 +0300

    feat: create component templates with reactive and template-driven forms, setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'reactive-form',
+        component: ReactiveFormComponent
+    },
+    {
+        path: 'template-driven-form',
+        component: TemplateDrivenFormComponent
+    },
+];
