e37be4d6ae09f5a4916910e7f8b96f5ed4a4db0e
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2025-03-03 01:44:25 +0300

    feat: set up auth reactive form

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, NonNullableFormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+  private fb = inject(NonNullableFormBuilder);
+  public form = this.fb.group({
+    email: new FormControl('e@mail.com'),
+    password: new FormControl('pass'),
+  });
 
+  constructor() {
+    this.form.valueChanges.subscribe(console.log);
+  }
 }
